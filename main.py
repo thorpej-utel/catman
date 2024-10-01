0217a71cd991d9be29f4bba59f75bcf3fe68966b
Jason Thorpe <jason.thorpe@utel.co.uk>
2024-10-01 17:39:17 +0100

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,30 @@
+"""
+
+This is the main file for your project.
+
+Create images, tilemaps, animations, and songs using the
+asset explorer in VS Code. You can reference those assets
+using the tagged templates on the assets namespace:
+
+assets.image`myImageName`
+assets.tilemap`myTilemapName`
+assets.tile`myTileName`
+assets.animation`myAnimationName`
+assets.song`mySongName`
+
+New to MakeCode Arcade? Try creating a new project using one
+of the templates to learn about Sprites, Tilemaps, Animations,
+and more! Or check out the reference docs here:
+
+https://arcade.makecode.com/reference
+
+"""
+# Code in this function will run once per frame. MakeCode
+# Arcade games run at 30 FPS
+
+def on_on_update():
+    pass
+game.on_update(on_on_update)
+
+scene.set_background_color(50)
+game.splash("Hello catman")
